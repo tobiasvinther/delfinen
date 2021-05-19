a45dc32aafdc2020ce7a2228bbcd6e4abc6a647a
victorvibe <victorvibe1998@gmail.com>
2021-05-19 16:37:21 +0200

    Årlig kontingent klasse

---
+++ src/accounting/Subscription.java
@@ -0,0 +1,15 @@
+package accounting;
+
+import members.*;
+
+public class Subscription {
+
+    //Det her skal være en metode hvor der bliver kaldt på arrayListen med medlemmere, så de enkeltes medlemmers kontingent
+    //bliver lagt sammen og udregnet
+    public static void calculateSubscription() {
+        MemberList.printMemberList();
+
+
+    }
+}
+
