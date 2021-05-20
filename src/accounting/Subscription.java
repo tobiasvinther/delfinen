e8953a2c10bfbab4796bf34cdbadec47fabca4e4
victorvibe <victorvibe1998@gmail.com>
2021-05-20 14:16:07 +0200

    Check fees- og has not payed metode færdig

--- src/accounting/Subscription.java
+++ src/accounting/Subscription.java
@@ -2,6 +2,7 @@ package accounting;
 
 import members.*;
 
+//@author Victor Vibe-Baasch
 public class Subscription {
 
     public static void calculateSubscription() {
@@ -10,9 +11,24 @@ public class Subscription {
         for (int i = 0; i < MemberList.getMemberArrayList().size(); i++ ) {
             total = total + MemberList.getMemberArrayList().get(i).getMembershipFee();
         }
-
-        System.out.println(total);
-
+        System.out.println("The approximate income from fees this year is: " + total);
     }
+
+    public static void hasNotPayed () {
+        for (int i = 0; i < MemberList.getMemberArrayList().size(); i++) {
+            if (MemberList.getMemberArrayList().get(i).isHasPaidMembership() != true ) {
+                System.out.println("People who needs to pay" + MemberList.getMemberArrayList().get(i));
+            }
+        }
+    }
+
+    public static void checkFees () {
+        PriceList priceList = new PriceList();
+        System.out.println("Junior fee: " + priceList.getJuniorFee());
+        System.out.println("Passive fee: " + priceList.getPassiveFee());
+        System.out.println("Senior fee: " + priceList.getSeniorFee());
+        System.out.println("Over 60 years fee: " + priceList.getOver60Fee());
+    }
+
 }
 
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 
+import accounting.PriceList;
 import accounting.Subscription;
 import members.Member;
 import members.MemberList;
@@ -15,10 +16,15 @@ public class Main {
         Member member2 = new Member("Flemming Nielsen", LocalDate.of(1994, 6, 11), "passiv", "");
         Member member3 = new Member("Anders Skovgaard", "16/08/1984", "passiv", "");
 
+        PriceList priceList = new PriceList();
+        Subscription subscription = new Subscription();
+
         MemberList.printMemberList();
         MemberList.searchForMember("Tobias Vinther");
         member1.printMember();
         Subscription.calculateSubscription();
+        Subscription.hasNotPayed();
+        subscription.checkFees();
 
     }
 }
