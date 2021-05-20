8f51c69c5ca347706ced0319bdae962e93e57f80
Thony <thon0101@stud.kea.dk>
2021-05-20 13:46:22 +0100

    New things added

--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,9 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Menu {
     public static void main(String[] args) {
+        mainMenu();
 
 
     }
@@ -11,38 +13,48 @@ public class Menu {
     //Appication application = new Application();
     //Members members = new Members();
 
-    public int getUserInput(){
+    public static int getUserInput(){
         Scanner scanner = null;
         int userInput = scanner.nextInt();
         return userInput;
     }
 
-    public void mainMenu() {
+    public static void mainMenu() {
         System.out.println();
-        System.out.println(">>>>>Welcome<<<<<");
-        System.out.println("1 >>> Create member");
-        System.out.println("2 >>> See memberlist");
-        System.out.println("3 >>> See pricelist");
-        System.out.println("4 >>> Exit");
+        System.out.println("+--------------------------+");
+        System.out.println("|         Welcome          |");
+        System.out.println("|     Menu Application     |");
+        System.out.println("+--------------------------+");
+        System.out.println("Please make a selection: ");
+        System.out.println("1 ->> Create new member");
+        System.out.println("2 ->> See memberlist");
+        System.out.println("3 ->> See pricelist");
+        System.out.println("4 ->> Exit program");
 
         switch (getUserInput()) {
             case 1:
+                //Create member
                 System.out.println("You chose to create a member");
                 System.out.println();
+                newMember();
                 break;
             case 2:
-                //
+                //See memberlist
+                System.out.println("You chose to see memberlist");
                 break;
             case 3:
-                //
+                //See pricelist
+                System.out.println("You chose to see pricelist");
                 break;
             case 4:
                 //Exit program
+                System.out.println("You chose to exit program");
             default:
-                //User input an unexspedted choise.
+                //User input an unexspected choise.
+                System.out.println("User input unexspected choise");
         }
     }
-    public void newMember() {
+    public static void newMember() {
         Scanner userInput = new Scanner(System.in);
         System.out.println("Enter member name: ");
         String name = userInput.nextLine();
