197abae3680e37038c5efd91ada73d424a0c3e83
Sonja <sonja_nawaz@hotmail.com>
2021-05-25 13:37:45 +0200

    Controller class added

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -4,44 +4,74 @@ import accounting.Membership;
 import accounting.PriceList;
 import members.Member;
 import members.MemberList;
-import java.time.LocalDate;
 
 public class Controller {
-    Member member = new Member();
-    MemberList memberList = new MemberList();
-    PriceList priceList = new PriceList();
 
     //Create new member
-    public Member newMember = new Member(String name, String birthday, String email, String membershipType, String activityType) {
-        Member newMember = new Member(name, birthday, email, membershipType, activityType);
-        return newMember;
+    public void newMember(Member newMember) {
+        MemberList.addMemberToList(newMember);
     }
 
     //Search for member
-    public void searchMember() {memberList.searchForMember();
+    public void searchMember(String name) {
+        MemberList.searchForMember(name);
     }
 
     //See memberlist
-    public void getMemberArraylist() {memberList.printMemberList();
+    public void printMemberList() {
+        MemberList.printMemberList();
+    }
+
+    //Print member
+    public void printMember(int index) {
+        MemberList.getMemberArrayList().get(index).printMember();
     }
 
     //Edit member
+    public void editName(Member member, String newName) {
+        member.setName(newName);
+    }
+    //Edit membershipType
+    public void editMembershipType(Member member, String newMembershipType) {
+        member.setMembershipType(newMembershipType);
+    }
+
+    //Edit activityType
+    public void editActivityType(Member member, String newActivityType) {
+        member.setActivityType(newActivityType);
+    }
+
+    //Edit email
+    public void editEmail(Member member, String newEmail) {
+        member.setEmail(newEmail);
+    }
 
     //Delete member
+    public void deleteMember(Member member) {
+        MemberList.deleteMemberFromList(member);
+    }
+
 
     //Edit passive membership
-    public void getPassivFee() {priceList.getPassiveFee(); }
+    public double getPassiveFee() {
+        return Membership.getMembershipFee("Passivt"); }
 
     //Edit junior membership
-    public void getJuniorFee() {priceList.getJuniorFee();}
+    public double getJuniorFee() {
+        return Membership.getMembershipFee("Junior"); }
+
 
     //Edit senior membership
-    public void getSeniorFee() {priceList.getSeniorFee(); }
+    public double getSeniorFee() {
+        return Membership.getMembershipFee("Senior"); }
 
     //Edit 60+ membership
-    public void getOver60Fee() {priceList.getOver60Fee(); }
+    public double getOver60Fee() {
+        return Membership.getMembershipFee("Senior over 60"); }
 
     //Edit 60+ membership discount
-    public void getOver60Discount() {priceList.getOver60Discount(); }
+    //public double getOver60Discount() {
+      //  return Membership.getMembershipFee("Senior rabat"); }
+
 
 }
